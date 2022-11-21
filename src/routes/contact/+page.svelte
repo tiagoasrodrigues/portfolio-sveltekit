17fdbfe8c368db525df3f7ed4ed4f45c49467366
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-11-21 14:15:20 -0300

    :tada: Initial project

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,20 @@
+<title>Contact | HSK</title>
+<link rel="stylesheet" href="./style.css">
+
+<script lang="ts">
+
+</script>
+
+<form>
+  <input name="name" type="text" placeholder="Nome">
+  <input name="email" type="email" placeholder="E-mail">
+  <br>
+  <input name="message" type="text" placeholder="Mensagem">
+  <br>
+  <button>Enviar</button>
+</form>
+
+<div>
+  <h1>Contact</h1>
+</div>
+<a href="/">Home</a>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,11 @@
+<title>Projects | HSK</title>
+<link rel="stylesheet" href="./style.css">
+
+<script lang="ts">
+
+</script>
+
+<div>
+  <h1>Projects</h1>
+</div>
+<a href="/">Home</a>
